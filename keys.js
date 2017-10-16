dcdc1a5c09ad97c7f7d9e6ff6b0e557738ec0f95
david morse <davidbryanmarshall@gmail.com>
2017-10-15 18:19:42 -0700

    set up for multiple key sets (twitter/spotify)

--- keys.js
+++ keys.js
@@ -4,7 +4,18 @@ var twitterKeys = {
   consumer_key: '8rjCvLAipq8n5Vs59K96vReVu',
   consumer_secret: 'jchBIW9ahheUASwKJy3iVTojPBztjyAAXfUpGWhxTnRTvzJEGC',
   access_token_key: '1656168907-UaWVBu8G89t2CM7iaf1YeFe7e2DVhavOIPTh377',
-  access_token_secret: 'A7QJ7gKClgenCGi1ZEK2KdFw66x8cKmHKpmbSoILfdZhf',
+  access_token_secret: 'A7QJ7gKClgenCGi1ZEK2KdFw66x8cKmHKpmbSoILfdZhf'
 }
 
-module.exports = twitterKeys;
+var spotifyKeys = {
+  client_id: '540b83f6b9254a85b547d636f4733bdd',
+  client_secret: 'a79490053b664f73be26dea9996b5395',
+}
+
+// module.exports = twitterKeys;
+
+module.exports = {
+  twitterKeys, 
+  spotifyKeys
+}
+
--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
-var twitterKeys = require('./keys.js');
+var appKeys = require('./keys.js');
 
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 var request = require('request');
 
 var command = process.argv[2];
@@ -24,18 +25,18 @@ switch (command) {
 function tweetIt() {
 
   var params = {screen_name: 'democracynow', count: 20};
-  var client = new Twitter(twitterKeys);
+  var client = new Twitter(appKeys.twitterKeys);
 
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
-      console.log('\nLast 20 Tweets from @' + params.screen_name + ': ');
-      console.log('----------------')
+      console.log('\nMost Recent 20 Tweets from @' + params.screen_name + ': ');
+      console.log('----------------\n')
 
       for (i = 0; i < tweets.length; i++) {
         console.log(i + 1);
         console.log(tweets[i].text);
         console.log(tweets[i].created_at);
-        console.log('----------------')
+        console.log('\n----------------\n')
       }
     }
   });
